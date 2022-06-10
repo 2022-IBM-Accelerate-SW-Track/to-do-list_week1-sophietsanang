cdc589b49721341efdef3f1e6e1a4e0c6254dbe5
Sophie Tsanang <sophietsanang@Sophies-Air.hsd1.md.comcast.net>
2022-06-09 22:04:22 -0400

    Making a new line

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import "./About.css";
+import headshot from "../assets/headshot.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+      <div class="split left">
+        <div className="centered">
+          <img 
+            className="profile_image"
+            src={headshot}
+            alt="Profile Pic"
+            ></img>
+        </div>
       </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Sophie Tsanang</div>
+          <div className="brief_description">
+            // I am a rising sophomore at Columbia University majoring in Computer Science with a minor in French. I loves improving my coding skills and watching new shows. //
+          </div>
+        </div>
+      </div>
+    </div>
     )
   }
 }
